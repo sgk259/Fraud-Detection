38fe731a519b657f6636bcafdd243c796f42d67f
sgk259 <gkgopikrishna37@gmail.com>
2023-07-24 21:21:49 +0530

    added new model file

---
+++ someModelCode.py
@@ -0,0 +1,13 @@
+print("something")
+
+def model1:
+    print("print in model1")
+
+def model2:
+    print("print in model2")
+
+def model3:
+    print("print in model3")
+
+
+print("end of models & model1 is selected")
